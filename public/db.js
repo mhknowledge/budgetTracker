d0b9c9c466997765301c69706acc2d008d9c27dd
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-07-25 17:01:42 -0400

    update to db.js file

--- public/db.js
+++ public/db.js
@@ -5,6 +5,7 @@ let budgetVersion;
 const request = indexedDB.open('BudgetDB', budgetVersion || 21);
 
 request.onupgradeneeded = function (e) {
+console.log('Upgrade needed in IndexDB');
 
   const { oldVersion } = e;
   const newVersion = e.newVersion || db.version;
